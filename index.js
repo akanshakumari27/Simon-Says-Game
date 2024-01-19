acd525c0a079fe43cf328aaab68872aeea53b875
akanshakumari27 <94097093+akanshakumari27@users.noreply.github.com>
2024-01-19 20:33:48 +0530

    Final TouchUp

--- index.js
+++ index.js
@@ -1,64 +1,90 @@
-const topLeft = document.querySelector('.top-left-panel');
-const topRight = document.querySelector('.top-right-panel');
-const bottomLeft = document.querySelector(
-  '.bottom-left-panel'
-);
-const bottomRight = document.querySelector(
-  '.bottom-right-panel'
-);
+let gameSeq=[];
+let userSeq=[];
+let btns=["red","green","blue","yellow"]
 
-const getRandomPanel = () => {
-  const panels = [
-    topLeft,
-    topRight,
-    bottomLeft,
-    bottomRight
-  ];
-  return panels[parseInt(Math.random() * panels.length)];
-};
+let started=false;
+let level=0;
 
-const sequence = [getRandomPanel()];
-let sequenceToGuess = [...sequence];
+let h3=document.querySelector("h3")
+let body = document.querySelector("body")
 
-const flash = panel => {
-  return new Promise(resolve => {
-    panel.className += ' active';
-    setTimeout(() => {
-      panel.className = panel.className.replace(
-        ' active',
-        ''
-      );
-      setTimeout(() => {
-        resolve();
-      }, 250);
-    }, 1000);
-  });
-};
-
-let canClick = false;
-
-const panelClicked = panelClicked => {
-  if (!canClick) return;
-  const expectedPanel = sequenceToGuess.shift();
-  if (expectedPanel === panelClicked) {
-    if (sequenceToGuess.length === 0) {
-      // start new round
-      sequence.push(getRandomPanel());
-      sequenceToGuess = [...sequence];
-      startFlashing();
+document.addEventListener("keypress",function(){
+    if(started==false){
+        console.log("game started")
+        started=true;
+        levelUp();
     }
-  } else {
-    // end game
-    alert('game over');
-  }
-};
+})
 
-const startFlashing = async () => {
-  canClick = false;
-  for (const panel of sequence) {
-    await flash(panel);
-  }
-  canClick = true;
-};
+function levelUp(){
+    userSeq = []  //as get the call for levelUp, means user completed it's all previous sequence(user.length = game.length) So, emptying it again
+    level++;
+    h3.innerText=`Level ${level}`
 
-startFlashing();
+    let randIdx = Math.floor(Math.random()*4)  //we get number excluding number written (0,1,2,3)
+    let randColors = btns[randIdx]
+    let randBtns = document.querySelector(`.${randColors}`)
+    gameSeq.push(randColors)
+    console.log(gameSeq)
+    flashGame(randBtns)
+}
+function flashGame(btn){
+    btn.classList.add("flashGame")  //this btn is in it's own functional scope
+    setTimeout(function(){
+        btn.classList.remove("flashGame")
+    },250)
+}  
+
+// *** Now this is till when game started and get 1st flash ***
+
+// *** Now when User press it's Key ***
+
+let allBtns = document.querySelectorAll(".two")
+for(btn of allBtns){   //this btn is in it's own functional scope
+    btn.addEventListener("click",btnPress)
+}
+function btnPress(){
+    console.log(this)
+    let btn=this
+    flashUser(btn)
+
+    userColor = btn.getAttribute("id")
+    console.log(userColor)
+    userSeq.push(userColor)
+
+    checkAns(userSeq.length-1) //delivering the last index number of user's button
+}
+function flashUser(btn){
+    btn.classList.add("flashUser")  //this btn is in it's own functional scope
+    setTimeout(function(){
+        btn.classList.remove("flashUser")
+    },250)
+}
+function checkAns(idx){   
+    if(userSeq[idx]===gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp,1000)
+        }
+    }else{
+        h3.innerHTML = `Game Over! Your Score is <b>${level}</b> <br>Press any key to Start again!` //innerHtml coz html tags won't apply in innerText
+        reset()
+        flashBackground()
+        //OR
+        // document.querySelector("body").style.backgroundColor="red"
+        // setTimeout(function(){
+        //     document.querySelector("body").style.backgroundColor = "rgb(113, 205, 208)"
+        // },250)
+    }
+}
+function reset(){
+    started = false
+    level = 0
+    gameSeq = []
+    userSeq = []
+}
+function flashBackground(){
+    body.classList.add("flashBody")
+    setTimeout( function(){
+        body.classList.remove("flashBody")
+    },250)
+}
